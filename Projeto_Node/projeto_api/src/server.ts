ab7e61ebeae895f80e75adb9ee2a7225deadb2bb
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2025-08-25 10:54:32 -0300

    Atualização Server.ts
    Importação e registro do Tratador Global de Erros.

--- Projeto_Node/projeto_api/src/server.ts
+++ Projeto_Node/projeto_api/src/server.ts
@@ -2,11 +2,12 @@
 import 'dotenv/config' // <-- ESSA DEVE SER A PRIMEIRA LINHA DO ARQUIVO
 import fastify from "fastify"
 import { userRoutes } from './modules/routes/user.controller' // 1. Importar as rotas
+import { errorHandler } from './lib/error.Handler' // 1. IMPORTE o novo handler
 
 const app = fastify({
     logger: true, // Liga o logger para vermos as requisições no terminal
 })
-
+app.setErrorHandler(errorHandler) // 2. Tratador de erros global
 app.register(userRoutes) // 2. Registrar o "plugin" de rotas
 
 // PRECISO SABER COMO ESSA FUNÇÃO FUNCIONA REALMENTE...
